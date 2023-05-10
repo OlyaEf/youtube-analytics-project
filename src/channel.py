f67517095bcb43cb0e47bdc3dca45f3e10cc5ab4
OlyaEf <efimovskikh@gmail.com>
2023-05-11 00:18:16 +0100

    Modification class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,39 +1,82 @@
+import json
 import os
 import googleapiclient.discovery
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для представления YouTube-канала.
+    """
     api_key: str = os.getenv('YT_API_KEY')
     channel_id: str = os.getenv('YT_CHANNEL_ID')
     youtube: googleapiclient.discovery.Resource = None
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала и api-ключом.
-        Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        """
+        Инициализирует экземпляр класса Channel.
+        Args:
+            channel_id (str): Идентификатор YouTube-канала.
+        Returns:
+            None
+        """
         self.youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=self.api_key)
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        response = self.youtube.channels().list(
-            part="snippet,statistics",
-            id=self.channel_id
-        ).execute()
-
+        self.channel_id = channel_id
+        response = self.youtube.channels().list(part="snippet,statistics", id=self.channel_id).execute()
         snippet = response['items'][0]['snippet']
         statistics = response['items'][0]['statistics']
+        self.id = self.channel_id
+        self.title = snippet['title']
+        self.description = snippet['description']
+        self.url = f"https://www.youtube.com/channel/{snippet['customUrl']}" if 'customUrl' in snippet \
+            else f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscribers_count = statistics['subscriberCount']
+        self.video_count = statistics['videoCount']
+        self.view_count = statistics['viewCount']
 
-        print(f"Title: {snippet['title']}")
-        print(f"Description: {snippet['description']}")
-        print(f"Custom URL: {snippet['customUrl']}")
-        print(f"Published At: {snippet['publishedAt']}")
-        print(f"View Count: {statistics['viewCount']}")
-        print(f"Subscriber Count: {statistics['subscriberCount']}")
-        print(f"Video Count: {statistics['videoCount']}")
+    def print_info(self) -> None:
+        """
+        Выводит в консоль информацию о канале.
+        Returns:
+            None
+        """
+        print(f"Title: {self.title}")
+        print(f"Description: {self.description}")
+        print(f"Custom URL: {self.url}")
+        print(f"Subscriber Count: {self.subscribers_count}")
+        print(f"Video Count: {self.video_count}")
+        print(f"View Count: {self.view_count}")
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API.
+        Returns:
+            googleapiclient.discovery.Resource: Объект для работы с YouTube API.
+        """
+        return googleapiclient.discovery.build("youtube", "v3", developerKey=cls.api_key)
+
+    def to_json(self, filename: str) -> None:
+        """
+        Сохраняет значения атрибутов экземпляра Channel в файл JSON.
+        Args:
+            filename (str): Имя файла для сохранения.
+        Returns:
+            None
+        """
+        data = {
+            'id': self.channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscribers_count': self.subscribers_count,
+            'videos_count': self.video_count,
+            'views_count': self.view_count,
+        }
+
+        with open(filename, 'w') as f:
+            json.dump(data, f)
 
 
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     vdud.print_info()
-
